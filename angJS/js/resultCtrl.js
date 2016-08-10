416babd53e7b56d1af5236166d2228d6222a8f50
alexwolfwang <alexwolfwang@gmail.com>
2016-08-10 15:16:00 -0400

    quizApp part7

--- angJS/js/resultCtrl.js
+++ angJS/js/resultCtrl.js
@@ -6,10 +6,28 @@
     angular.module('doraemonFact')
         .controller('resultCtrl', resultController);
 
-        resultController.$inject = ['quizMetrics'];
+        resultController.$inject = ['quizMetrics','dataService'];
 
-        function resultController(quizMetrics) {
+        function resultController(quizMetrics,dataService) {
             var vm = this;
             vm.quizMetrics = quizMetrics;
+            vm.dataService = dataService;
+            vm.getAnswerClass = getAnswerClass;
+            vm. activeQuestion = 0;
+
+
+            function getAnswerClass(index) {
+                if(index === quizMetrics.correctAnswer[vm.activeQuestion]){
+                    return "bg-success";
+                } else if(index === dataService.quizQuestion[vm.activeQuestion].selected) {
+                    return "bg-danger";
+                }
+            }
         }
+
+
+
+
+
+
 })();
